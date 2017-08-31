43ae1ad650aa13dca4b400aa86f99aafe93b26ec
stopvoldemort <david.squires1@gmail.com>
2017-08-31 08:51:23 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split("").last=="."
   end
 
   def question?
-
+    self.split("").last=="?"
   end
 
   def exclamation?
-
+    self.split("").last=="!"
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).reject{|array| array.empty?}.length
   end
 end
